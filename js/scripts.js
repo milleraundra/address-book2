065f8fe4c2d9dd1a1390c551cc9c523f9874daae
aundra miller and mike gallegos <mklgallegos@gmail.com>
2016-01-25 13:25:51 -0800

    added functionality for multiple addresse

--- js/scripts.js
+++ js/scripts.js
@@ -16,18 +16,49 @@ function Address(street, city, state) {
 	this.state = state;
 }
 
+Address.prototype.fullAddress = function() {
+	return this.street + ", " + this.city + ", " + this.state;
+}
+
 
 // UI Logic
 
 $(document).ready(function() {
+	$("#add-address").click(function() {
+    $("#new-addresses").append('<div class="new-address">' +
+       '<div class="form-group">' +
+         '<label for="new-street">Street</label>' +
+         '<input type="text" class="form-control new-street">' +
+       '</div>' +
+       '<div class="form-group">' +
+         '<label for="new-city">City</label>' +
+         '<input type="text" class="form-control new-city">' +
+       '</div>' +
+       '<div class="form-group">' +
+         '<label for="new-state">State</label>' +
+         '<input type="text" class="form-control new-state">' +
+       '</div>' +
+     '</div>');
+  });
+
 	$("form#new-contact").submit(function(event) {
 		event.preventDefault();
 
 		var inputtedFirstName = $("input#new-first-name").val();
 		var inputtedLastName = $("input#new-last-name").val();
-		var newContact = new Contact(inputtedFirstName, inputtedLastName);
+		 newContact = new Contact(inputtedFirstName, inputtedLastName);
+
+		$(".new-address").each(function() {
+  var inputtedStreet = $(this).find("input.new-street").val();
+  var inputtedCity = $(this).find("input.new-city").val();
+  var inputtedState = $(this).find("input.new-state").val();
+  var newAddress = new Address(inputtedStreet, inputtedCity, inputtedState)
+  newContact.addresses.push(newAddress)
+});
 
 		$("ul#contacts").append("<li><span class='contact'>" + newContact.fullName() + "</span></li>");
+
+
 
 		// this code clears the form fields
 		$("input#new-first-name").val("");
@@ -35,6 +66,11 @@ $(document).ready(function() {
 
 
 		$(".contact").last().click(function() {
+			$("ul#addresses").text("");
+			newContact.addresses.forEach(function(address) {
+  			$("ul#addresses").append("<li>" + address.fullAddress() + "</li>");
+			});
+
 		$("#show-contact").show();
 		$("#show-contact h2").text(newContact.fullName());
 		$(".first-name").text(newContact.firstName);
--- spec/specs.js
+++ spec/specs.js
@@ -19,4 +19,9 @@ describe('Address', function() {
 		expect(testAddress.city).to.equal("Test City");
 		expect(testAddress.state).to.equal("Test State");
 	});
+	
+	it("adds the fullAddress method to all addresses", function() {
+		var testAddress = new Address("123 Main St", "Test City","Test State");
+		expect(testAddress.fullAddress()).to.equal("123 Main St, Test City, Test State");
+	});
 });
